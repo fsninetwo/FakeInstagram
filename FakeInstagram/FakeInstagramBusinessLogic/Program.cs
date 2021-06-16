98388d60521b407101e07f78d6862fd28fc0427a
fsninetwo <32400063+fsninetwo@users.noreply.github.com>
2021-06-16 14:11:54 +0300

    ViewModel and BusinessLogic
    Creating 2 new projects.

---
+++ FakeInstagram/FakeInstagramBusinessLogic/Program.cs
@@ -0,0 +1,15 @@
+﻿using FakeInstagramEfModels.Entities;
+using FakeInstagramMigrations;
+using System;
+using System.Collections.Generic;
+
+namespace FakeInstagramBusinessLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
